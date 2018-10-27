ce840e93cb57e02f0251504d5e664ce9e736e14b
sun5hine <af5kh@yahoo.com>
2018-10-27 22:49:52 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -36,9 +36,18 @@ counter
 end
 
 def get_occupation(data, hometown)
-  
-end
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          return hash["occupation"]
+        end
+      end
+    end
+  end
+ end	
+
 
 def get_average_age_for_season(data, season)
-  # code here
+  
 end
